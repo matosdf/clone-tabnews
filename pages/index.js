09f144532c1977939b3e0575ef29ed76fe90a66d
Danilo Matos <danilo.fmatos@yahoo.com.br>
2025-12-25 11:20:47 -0300

    Mudança de frase para apresentação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home (){
-    return <h1>Te amo muito</h1>
+function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 to-red-500">
+      <h1 className="text-4xl font-bold text-white text-center p-8 rounded-lg shadow-lg animate-pulse">
+        Meu anjo, começaremos agora em 2026 uma nova etapa de nossas vidas, duas bebês lindas para cuidar e seus negócios para gerenciar.
+        O desafio é grande, mas conte comigo para o que precisar porque juntos e sob a bênção de papai do Céu vamos, tenho certeza que vamos ir bem longe.
+        Te amo muito!
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
