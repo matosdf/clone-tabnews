044da36ba55106c3e5d8c60b0024fe0e0a7e8a7e
Danilo Matos <danilo.fmatos@yahoo.com.br>
2025-12-25 11:47:14 -0300

    correção da mensagem

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 to-red-500">
       <h1 className="text-4xl font-bold text-white text-center p-8 rounded-lg shadow-lg animate-pulse">
         Meu anjo, começaremos agora em 2026 uma nova etapa de nossas vidas, duas bebês lindas para cuidar e seus negócios para gerenciar.
-        O desafio é grande, mas conte comigo para o que precisar porque juntos e sob a bênção de papai do Céu vamos, tenho certeza que vamos ir bem longe.
+        O desafio é grande, mas conte comigo para o que precisar porque juntos e sob a bênção de Papai do Céu, tenho certeza que vamos ir bem longe.
         Te amo muito!
       </h1>
     </div>
